38b3a08550f233b88957bf3b3e11bb7ebd7d5eff
stianst <stianst@gmail.com>
2024-09-10 12:33:05 +0200

    Fixes

--- kc-test-server.sh
+++ kc-test-server.sh
@@ -1,17 +1,22 @@
-#!/bin/bash
+#!/bin/bash -e
 
 VERSION=25.0.4
 
-rm -rf test-server/
-mkdir test-server
+rm -rf test-server/keycloak-$VERSION
+mkdir -p test-server
 
-unzip ~/dev/dl/keycloak-$VERSION.zip -d test-server/
-mkdir -p test-server/keycloak-$VERSION/data/import
+cd test-server
+if [ ! -f keycloak-$VERSION.zip ]; then
+    echo "Downloading https://github.com/keycloak/keycloak/releases/download/$VERSION/keycloak-$VERSION.zip"
+    curl -O -L https://github.com/keycloak/keycloak/releases/download/$VERSION/keycloak-$VERSION.zip
+fi
+unzip keycloak-$VERSION.zip
 
-cp src/test/resources/testrealm.json test-server/keycloak-$VERSION/data/import/
+mkdir -p keycloak-$VERSION/data/import
+cp ../src/test/resources/testrealm.json keycloak-$VERSION/data/import/
 
 export DEBUG=true
 export KEYCLOAK_ADMIN=admin
 export KEYCLOAK_ADMIN_PASSWORD=admin
 
-test-server/keycloak-$VERSION/bin/kc.sh start-dev --import-realm --cache=local --features=admin-fine-grained-authz,token-exchange --log=file
+keycloak-$VERSION/bin/kc.sh start-dev --import-realm --cache=local --features=admin-fine-grained-authz,token-exchange --log=file
