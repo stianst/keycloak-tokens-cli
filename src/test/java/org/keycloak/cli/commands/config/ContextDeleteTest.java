828dacd42e65ed6568d3f906250d59be38c16beb
Stian Thorgersen <stianst@gmail.com>
2024-10-21 08:50:49 +0200

    Delete tokens when deleting a context (#90)
    Closes #55

--- src/test/java/org/keycloak/cli/commands/config/ContextDeleteTest.java
+++ src/test/java/org/keycloak/cli/commands/config/ContextDeleteTest.java
@@ -8,8 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.keycloak.cli.ConfigTestProfile;
 import org.keycloak.cli.assertion.LauncherAssertions;
+import org.keycloak.cli.oidc.Tokens;
+import org.keycloak.cli.tokens.TokenStore;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @QuarkusMainTest
 @ExtendWith(ConfigTestProfile.class)
@@ -17,9 +20,14 @@ public class ContextDeleteTest {
 
     @Test
     public void testDeleteContext(QuarkusMainLauncher launcher) throws IOException {
+        TokenStore tokenStore = new TokenStore();
+        tokenStore.getTokens().put("test-password", new Tokens("refresh", Collections.emptySet(), "access", "id", Collections.emptySet(), 12L));
+        ConfigTestProfile.getInstance().updateTokens(tokenStore);
+
         LaunchResult result = launcher.launch("config", "context", "delete", "--context=test-password");
         LauncherAssertions.assertSuccess(result, "Context 'test-password' deleted");
         Assertions.assertNull(ConfigTestProfile.getInstance().loadConfig().getIssuers().get("test-issuer").getContexts().get("test-password"));
+        Assertions.assertFalse(ConfigTestProfile.getInstance().getTokensFile().isFile());
     }
 
 }
